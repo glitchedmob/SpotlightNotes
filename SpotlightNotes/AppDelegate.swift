bc84f6d6425d73f9a3ff8b80e0e29ea9d5853acd
Levi Zitting <me@levizitting.com>
2023-09-16 21:58:08 -0500

    feat: add global keyboard shortcut for showing window

--- SpotlightNotes/AppDelegate.swift
+++ SpotlightNotes/AppDelegate.swift
@@ -6,17 +6,23 @@
 //
 
 import Cocoa
+import KeyboardShortcuts
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     private var window: NSWindow!
+
+    private var isVisible = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         NSApp.setActivationPolicy(.accessory)
 
         window = NSWindow(contentRect: NSRect(x: 0, y: 0, width: 480, height: 270), styleMask: [], backing: .buffered, defer: false)
         window.center()
-        window.makeKeyAndOrderFront(nil)
         window.level = .floating
+
+        KeyboardShortcuts.onKeyUp(for: .showFloatingPanel) { [weak self] in
+            self?.toggleWindow()
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -26,4 +32,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+
+
+    private func toggleWindow() {
+        if (isVisible) {
+            window.orderOut(self)
+        } else {
+            window.makeKeyAndOrderFront(self)
+            NSApp.activate(ignoringOtherApps: true)
+        }
+
+        isVisible = !isVisible
+    }
+}
+
+extension KeyboardShortcuts.Name {
+    static let showFloatingPanel = Self("showFloatingPanel", default: .init(.space, modifiers: [.option]))
 }
